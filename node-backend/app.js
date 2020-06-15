d2ad02369ec03a6b1d7be1e8e09a2db128707161
jonadepr <jonadepr@gmail.com>
2020-06-15 12:32:26 +0200

    node back end inicial

---
+++ node-backend/app.js
@@ -0,0 +1,10 @@
+const express = require("express");    // Utilizamos express
+const app = express();
+
+
+
+
+
+
+
+module.exports = app;
---
+++ node-backend/server.js
@@ -0,0 +1,21 @@
+// Inicialización
+const mongoose = require("mongoose");  // mongoose para mongodb
+const app = require("./app");
+
+
+app.use(express.json()); // para entender json
+
+app.use('/api/user', require('./routes/User'));
+app.use('/api/rol', require('./routes/Rol'));   // donde coge las rutas estaaaaa
+// Configuracion
+
+mongoose
+    .connect(
+        "mongodb+srv://admin:admin@cluster0-rckpe.mongodb.net/market-store-jedi?retryWrites=true&w=majority"
+    )
+    .then((res) =>
+        app.listen(4000, () => {
+            console.log("mongodb is running");
+        })
+    )
+    .catch((err) => console.log("se fue a la mierda"));
